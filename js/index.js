5b4659c39debffad4a0841254ec81b55c5060a45
ranieropascual <89618973+ranieropascual@users.noreply.github.com>
2022-11-03 21:21:38 -0300

    -

--- js/index.js
+++ js/index.js
@@ -1,11 +1,36 @@
-import Model from './model.js';
-import View from './view.js';
+document.addEventListener('DOMContentLoaded', function(){
+  const btn = document.getElementById('add');
+  const title = document.getElementById('title');
+  const table = document.getElementById('table');
+  const alert = document.getElementById('alert');
+  const description = document.getElementById('description');
 
-document.addEventListener('DOMContentLoaded', () => {
-  const model = new Model();
-  const view = new View();
-  model.setView(view);
-  view.setModel(model);
+  function addTodo () {
+    if (title.value === '' || description.value === '') {
+      alert.classList.remove('d-none');
+      alert.innerText = 'title and description are required';
+    }else{
+      console.log('ok');
+    }
 
-  view.render();
+    alert.classList.add('d-none');
+    const row = table.insertRow();
+    row.innerHTML = `
+    <td>${title.value}</td>
+    <td>${description.value}</td>
+    <td class= "text-right">
+    <button class="btn btn-primary mb-1">
+    <i class="btn btn-danger mb-1 ml-1">
+    <i class="fa fa-pencil"></i>
+    </button>
+    <button class="btn btn-danger mb-1 ml-1">
+    <i class="fa fa-trash"></i>
+    </button>
+    </td>
+    `;
+  }
+
+  btn.onclick = addTodo; 
 });
+
+
